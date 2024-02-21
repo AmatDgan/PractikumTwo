56ecac60b54ea1a0d42394c3bb3ca44180414364
AmatDgan <159452919+AmatDgan@users.noreply.github.com>
2024-02-21 09:24:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+x = 96
+y = x//16
+z = int(input())
+c = (z - 48 * x)//y
+print(c)
+
+x = input().split()
+print(x, sep='\n')
+
+S, R = map(int, input().split())
+print(S + R)
+
+kittens = 7 * 7 * 7 * 7
+cats = 7 * 7 * 7
+wives = 7
+print(kittens + cats + wives + 2)
+
+x = int(input())
+y = int(x * 0.19 * 100) / 100
+print(y)
+
+m = float(input())
+r = float(input())
+m = m * 0.45359237
+r = r * 0.0254
+IMT = m / (r**2)
+print(IMT)
+
+print((10000 * 0.01) * 1000)
+
+N, M = map(int, input().split())
+print(M // (N+1))
+
+N, K = map(int, input().split())
+print(N % K)
+
+m = int(input())
+m = m / 1000
+print(m / 1.609344)
